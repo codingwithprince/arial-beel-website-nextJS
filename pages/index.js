728c9fa68ce6d7a25eaf2972942a52effae97524
Prince Ahmed <codingwithprince@gmail.com>
2021-10-10 09:55:29 +0600

    added  meta tags

--- pages/index.js
+++ pages/index.js
@@ -11,30 +11,44 @@ import Seasons from '../components/Seasons';
 
 export default function Home() {
   return (
-   <>
-    {/* Head */}
-    <Head>
-      <title>Arial Beel - The Third Largest Wetland Of Bangladesh</title>
-      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" /> 
-      <meta name="keyword" content="arial beel, munshiganj, alampur tourist places, sreenagar" />
-      <meta name="description" content="This website is all about Arial Beel ( আড়িয়াল বিল ). Arial Beel is the third largest wetland in Bangladesh Which is situated in Sreenagar Upazila of Munshiganj District. আড়িয়াল বিল ভ্রমন." />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="google-site-verification" content="4Qg7xxWvxPJN4xTfO_0tVbJnJ5vvPYiUwMpzxuULhCU" />
-      <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
-    </Head>
-    {/* === Hero section === */}
-    <Hero />
-    {/* === Seasons Section === */}
-    <Seasons />
-    {/* === Image Gallery Section === */}
-    <ImageGallery />
-    {/* === Location Section === */}
-    <Location />
-    {/* === About Section === */}
-    <About />
-    {/* === Guide Section === */}
-    <Guide />
+    <>
+      {/* Head */}
+      <Head>
+        <title>Arial Beel - The Third Largest Wetland Of Bangladesh</title>
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="keyword" content="arial beel,arial beel airport,
+              arial beel wikipedia,
+              arial beel map,
+              arial beel location,
+              arial beel natural resources,
+              Dhaka to arial beel,
+              how to go arial beel from dhaka,
+              arial bill location,
+              arial beel tour guide,
+              arial beel bangladesh,
+              arial beel munshiganj
+              arial beel sreenagar,
+              how to visit arial beel,
+              hire boat in arial beel,
+              alampur arialbeel" />
+        <meta name="description" content="This website is all about Arial Beel ( আড়িয়াল বিল ). Arial Beel is the third largest wetland in Bangladesh Which is situated in Sreenagar Upazila of Munshiganj District. আড়িয়াল বিল ভ্রমন." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="4Qg7xxWvxPJN4xTfO_0tVbJnJ5vvPYiUwMpzxuULhCU" />
+        <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+      </Head>
+      {/* === Hero section === */}
+      <Hero />
+      {/* === Seasons Section === */}
+      <Seasons />
+      {/* === Image Gallery Section === */}
+      <ImageGallery />
+      {/* === Location Section === */}
+      <Location />
+      {/* === About Section === */}
+      <About />
+      {/* === Guide Section === */}
+      <Guide />
 
-   </>
+    </>
   )
 }
